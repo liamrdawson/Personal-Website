02851876b4c52dedace9fff1085eae2c3e34a4f1
Liam Dawson <liamrdawson@gmail.com>
2019-03-03 01:45:17 +0000

    replace skills grid with flexbox, add JQuery interactivity on click, add card expander

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hi, from script.js');
+
+
+var $cell = $('.card');
+
+$cell.find('.js-expander').click(function() {
+
+    var $thisCell = $(this).closest('.card');
+  
+    if ($thisCell.hasClass('is-collapsed')) {
+      $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+      $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+      
+      if ($cell.not($thisCell).hasClass('is-inactive')) {
+        //do nothing
+      } else {
+        $cell.not($thisCell).addClass('is-inactive');
+      }
+  
+    } else {
+      $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+      $cell.not($thisCell).removeClass('is-inactive');
+    }
+  });
