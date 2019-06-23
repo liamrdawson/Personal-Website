332b12f8879d166f21901de3f585da1c99ebe3ba
Liam Dawson <liamrdawson@gmail.com>
2019-06-23 14:59:25 +0100

    load photography project folder with source files

---
+++ photography-site/JS/script.js
@@ -0,0 +1,24 @@
+
+//  ANIMATE NAV SLIDER ANIMATIONS ON BURGER MENU CLICK
+
+const navSlide = () => {
+    const burger = document.querySelector(`.burger`);
+    const nav = document.querySelector(`nav`);
+    const navLinks = document.querySelectorAll(`nav ul li`);
+    burger.addEventListener(`click`, () => {
+        //  Toggle NAV
+        nav.classList.toggle(`active`);
+        //  Fade links on reveal
+        navLinks.forEach( (link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ``;
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .8}s`;
+            }
+        });
+        //  Burger to X animation
+         burger.classList.toggle(`toggle`);
+    });
+}
+
+navSlide();
